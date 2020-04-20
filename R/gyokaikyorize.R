82cccd2c1ea5321124b44394a18952332fa3d724
JK-junkin <kinoshitaj@affrc.go.jp>
2020-04-20 15:53:38 +0900

    :sparkles: Add S3method for gyokaikyorize()

--- R/gyokaikyorize.R
+++ R/gyokaikyorize.R
@@ -1,10 +1,42 @@
+#' Format and wrangle catch data to fit business requirements
 #'
+#' @param df a data frame to be processed
+#' @param species species name. Only supported with 'maaji' for now.
 #'
-#' @return
+#' @return formatted data.frame
 #'
 #' @examples
+#' \dontrun{
+#' gyokaikyorize(df = data.frame())
+#' }
 #' @export
 gyokaikyorize <- function(df, species) {
+  if (missing(species) || length(species) == 0) {
+    stop("Specify `species` such as 'maaji', 'maiwashi', 'masaba', etc..")
+  }
+
+  if (!requireNamespace("stockdbr", quietly = T)) {
+    stop(
+      paste("R pkg `stockdbr` will be needed if `df` is missing.",
+            "Please install it from GitHub.",
+            "remotes::install_github(JK-junkin/stockdbr)"),
+      call. = F)
+  }
+
+  if (missing(df)) {
+    message("stockdbr::load_catch() loaded catch database automatically.")
+    df <- stockdbr::load_catch()
+  } else {
+    df
+  }
+
   class(df) <- append(class(df), species)
-  UseMethod("gyokaikyorize")
+  UseMethod("gyokaikyorize", df)
+}
+
+#' @export
+gyokaikyorize.default <- function(df, species) {
+  if (species != "maaji") {
+    stop("Argument `species` only supports 'maaji' for now.")
+  }
 }
--- R/gyokaikyorize_maaji.R
+++ R/gyokaikyorize_maaji.R
@@ -1,4 +1,9 @@
+#' @param df a data frame
+#' @param species 'maaji'
 #' @export
-gyokaikyorize.maaji <- function(df) {
+gyokaikyorize.maaji <- function(df, species) {
+  if (missing(df)) df <- stockdbr::load_catch() else df
 
+  df %>%
+    dplyr::filter(Species == "マアジ")
 }
